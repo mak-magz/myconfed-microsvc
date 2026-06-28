97aae5a3701c22d7c4a9b8651706965ed841726d
Mak Magz <marktmagcanta.dev@gmail.com>
2026-06-29 02:18:32 +0800

    adds domain layer

--- backend/services/user/internal/domain/user.go
+++ backend/services/user/internal/domain/user.go
@@ -1,1 +1,45 @@
 package domain
+
+import (
+	"errors"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+}
+
+var (
+	ErrEmailRequired    = errors.New("email is required")
+	ErrPasswordRequired = errors.New("password is required")
+)
+
+func NewUser(
+	id,
+	email,
+	password string,
+) (*User, error) {
+
+	user := &User{
+		ID:       id,
+		Email:    email,
+		Password: password,
+	}
+
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *User) Validate() error {
+	if u.Email == "" {
+		return ErrEmailRequired
+	}
+	if u.Password == "" {
+		return ErrPasswordRequired
+	}
+	return nil
+}
